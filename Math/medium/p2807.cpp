ecf1c9cde6db166cb9945a930ed44e244c440fa4
sanin7k <dev.sanink@gmail.com>
2025-10-25 17:20:09 +0530

    added

---
+++ Math/medium/p2807.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *   int val;
+ *   ListNode *next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* insertGreatestCommonDivisors(ListNode* head) {
+    ListNode* temp = head;
+    while (temp->next) {
+      ListNode* newNode = new ListNode(gcd(temp->val, temp->next->val), temp->next);
+      temp->next = newNode;
+      temp = temp->next->next;
+    }
+    return head;
+  }
+};
+
