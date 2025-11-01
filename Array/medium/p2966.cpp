01ba1eaeb8c680e80aad922ca91f301025e557b7
sanin7k <dev.sanink@gmail.com>
2025-11-01 13:34:35 +0530

    added

---
+++ Array/medium/p2966.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  vector<vector<int>> divideArray(vector<int>& nums, int k) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    vector<vector<int>> ans;
+    ans.reserve(n / 3);
+
+    for (int i = 0; i < n - 2; i += 3) {;
+      if (nums[i + 2] - nums[i] > k)
+        return {};
+      ans.push_back({nums[i], nums[i + 1], nums[i + 2]});
+    }
+    return ans;
+  }
+};
+
