7a8241f3bc2968f2d17798902287499f55d3043b
sanin7k <dev.sanink@gmail.com>
2025-11-10 18:10:45 +0530

    added

---
+++ Array/medium/p1441.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  vector<string> buildArray(vector<int>& target, int n) {
+    vector<string> ans;
+    int k = 0;
+    for (int i = 1; i <= n && k < target.size(); ++i) {
+      ans.push_back("Push");
+      if (target[k] == i) k++;
+      else ans.push_back("Pop");
+    }
+    return ans;
+  }
+};
+
