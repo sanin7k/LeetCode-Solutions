dbd9418abc67beef3fabd0795063573dafc8f3ee
sanin7k <dev.sanink@gmail.com>
2025-11-10 21:31:38 +0530

    added

---
+++ String/medium/p1910.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+  string removeOccurrences(string s, string part) {
+    while (!s.empty() && s.find(part) < s.size()) {
+      s.erase(s.find(part), part.size());
+    }
+    return s;
+  }
+};
+
--- String/medium/p2390.cpp
+++ String/medium/p2390.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
   string removeStars(string s) {
-    vector<char> st;
-    for (auto& x : s) {
-      if (x != '*') st.push_back(x);
-      else st.pop_back();
-    }
     string ans = "";
-    for (auto& x : st)
-      ans += x;
+    for (auto& x : s) {
+      if (x != '*') ans.push_back(x);
+      else ans.pop_back();
+    }
     return ans;
   }
 };
